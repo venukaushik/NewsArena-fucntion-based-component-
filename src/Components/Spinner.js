df7d1c4b00ce6eee744c693cc1bfc177614b2c5c
venukaushik <venukaushik31@gmail.com>
2024-02-18 12:44:09 +0530

    .

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import loading from './loading.gif'
+import React from "react";
+import loading from "./loading.gif";
 const Spinner = () => {
+  return (
+    <div className="text-center">
+      <img className="my-3" src={loading} alt="loading" />
+    </div>
+  );
+};
 
-    return (
-        <div className='text-center'>
-            <img className='my-3' src={loading} alt="loading" />
-        </div >
-    )
-
-}
-
-export default Spinner
+export default Spinner;
